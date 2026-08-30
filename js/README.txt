e2f31aaba2c4318abf7025c2376bb575617bc7d0
BruceLi21 <bluecn21@gmail.com>
2026-08-30 16:36:10 +0800

    Add files via upload

---
+++ js/README.txt
@@ -0,0 +1,1 @@
+V19 JavaScript 目前內嵌於 index.html。後續若拆檔，遊戲程式放置於此。
---
+++ 啟動遊戲.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+chcp 65001 >nul
+cd /d "%~dp0"
+echo.
+echo 無盡仙途 V19 本機伺服器
+echo 網址：http://localhost:8000/?v=19
+echo.
+start "" http://localhost:8000/?v=19
+py -m http.server 8000
+if errorlevel 1 python -m http.server 8000
+pause
